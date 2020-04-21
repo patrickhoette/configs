878a13fc2ed6e2a76bb4d9842023fdf2545d4d19
Patrick Hoette <p.hoette@m2mobi.com>
2020-04-21 13:18:08 +0200

    Remove OMF as there installer is broken and there seems to be little affort fixing it

--- Fish/setup.sh
+++ Fish/setup.sh
@@ -4,22 +4,12 @@ editor='vim'
 
 echo "Setting up fish"
 
-echo "Installing fisher"
-curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-
-echo "Installing oh-my-fish"
-curl -L https://get.oh-my.fish | fish
-
-echo "Installing krisleech theme"
-omf install krisleech
-
 echo "Copying over configurations"
 cp -r completions ~/.config/fish/
 cp -r conf.d ~/.config/fish/
 cp config.fish ~/.config/fish/
 cp fish_variables ~/.config/fish/
 cp fishfile ~/.config/fish/
-cp -r fisher ~/.config/fisher/
 cp -r functions ~/.config/fish/
 
 echo "Setting default editor"
